4e949c0cd8f8a03539444b8c7cec0c78be2f5294
Chusensha <35710255+Chusensha@users.noreply.github.com>
2020-10-17 23:12:39 +0300

    Update geoweather.js

--- geoweather.js
+++ geoweather.js
@@ -9,7 +9,7 @@ class Geoweather {
 
     // Fetch weather for COORDINATES from API as promise
     async getWeather() {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${this.latitude}&lon=${this.longitude}&units=${this.units}&lang=${this.lang}&appid=76b93cbb410678929b546b3dcd258ec6`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.latitude}&lon=${this.longitude}&units=${this.units}&lang=${this.lang}&appid=76b93cbb410678929b546b3dcd258ec6`);
 
         const responseData = await response.json();
         return responseData;
